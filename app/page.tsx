93a649317df886cbafb7afdd0b054e7160a22d47
doublejd <jd.jeong@lgepartner.com>
2024-04-25 07:50:42 +0900

    git 테스트 완료

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import React, { useEffect, useState } from "react";
 import LoadingPage from "./[lang]/components/loadingSkeleton/loadingPage";
 
 export default function Home() {
-  //git test
   useEffect(() => {
     const lang = localStorage.getItem("latsNoise_i18nextLng");
     const locale_code = localStorage.getItem("locale_code");
