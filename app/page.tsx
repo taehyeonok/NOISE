34a05d2759974f71b156fe01ed7ba276cd40a9fe
doublejd <jd.jeong@lgepartner.com>
2024-04-25 07:49:43 +0900

    불필요한 주석  제거

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,10 @@ import React, { useEffect, useState } from "react";
 import LoadingPage from "./[lang]/components/loadingSkeleton/loadingPage";
 
 export default function Home() {
-  // const [init, setInit] = useState<boolean>(false);
   useEffect(() => {
-    // setInit(true);
     const lang = localStorage.getItem("latsNoise_i18nextLng");
     const locale_code = localStorage.getItem("locale_code");
     redirect(`/${locale_code ? locale_code : lang}/container/input`);
   }, []);
-  // redirect(`/containers/main`);
   return <LoadingPage />;
 }
