e752c0ae5dad12299560bf8515d3e6da633c7094
taehyeonok <157551292+taehyeonok@users.noreply.github.com>
2024-09-02 12:31:16 +0900

    버그 수정

--- app/[lang]/container/layout.tsx
+++ app/[lang]/container/layout.tsx
@@ -1,21 +1,20 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import Header from "@/app/[lang]/components/header/header";
 import Footer from "@/app/[lang]/components/footer/footer";
 import { useParams } from "next/navigation";
 import { getSelectedLanguage } from "@/app/utils/utils";
 import { ProjectInfoProvider } from "@/app/context/projectInfoContext";
-import { useTranslation } from "@/app/i18n/client";
+import LoadingPage from "../components/loadingSkeleton/loadingPage";
 
 export default function MenuLayout({ children }: { children: React.ReactNode }) {
   const param = useParams<{ lang: string }>();
   const selectedLanguage = getSelectedLanguage(param.lang);
-  const { t } = useTranslation(param.lang);
   return (
     <div className={"page"}>
       <Header lang={param.lang} selectedLanguage={selectedLanguage} />
       <ProjectInfoProvider>
-        {children}
+        <Suspense fallback={<LoadingPage />}>{children}</Suspense>
         <Footer />
       </ProjectInfoProvider>
     </div>
